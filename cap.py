270df9b9367a833b1bdbb5c999c7f90c2d5ce6e6
HASSAN BARI RAZA <hbr8218@gmail.com>
2020-02-25 17:42:40 +0530

    captures pictures in real time

--- cap.py
+++ cap.py
@@ -3,34 +3,39 @@ import numpy
 
 face_cascade = cv2.CascadeClassifier('haarcascade_frontalface_default.xml')
 
-cap = cv2.VideoCapture(0)
+cap  = cv2.VideoCapture(0)
+total_frame = 0
+id = 0
 
-# Check if the webcam is opened correctly
 if not cap.isOpened():
-    raise IOError("Cannot open webcam")
+    exit(0)
+
+#Capture images per 25 frame
+frameFrequency=25
 
 while True:
-    id=1
     ret, frame = cap.read()
-    # print(frame)
+    if ret is False:
+        break
     gray =cv2.cvtColor(frame,cv2.COLOR_BGR2GRAY)
     face = face_cascade.detectMultiScale(gray,1.3,6)
     
-    # cv2.imwrite('frame'+str(id)+'.png',face)
-
     for (x,y,w,h) in face:
-        
         cv2.rectangle(frame,(x,y),(x+w,y+h), (0,0,255), 2)
-        # roi_gray = gray[y:y+h, x:x+w]
-        # roi_color = frame[y:y+h, x:x+w]
-        # frame = cv2.resize(frame, None, fx=0.5, fy=0.5, interpolation=cv2.INTER_AREA)
-
-    cv2.imshow('Input', frame)
-    
-
+        id += 1
+        image_name = 'my_pic' + str(id) +'.jpg'
+        cv2.imwrite(image_name, frame)
+        cv2.imshow('Input',frame)
+        print(image_name)
+    # total_frame += 1
+    # if total_frame%frameFrequency == 0:
+    #     id += 1
+    #     image_name = 'my_pic' + str(id) +'.jpg'
+    #     cv2.imwrite(image_name, frame)
+    #     cv2.imshow('Input',frame)
+    #     print(image_name)
     c = cv2.waitKey(1)
     if c == 27:
         break
-
-cap.release()
+cv2.release()
 cv2.destroyAllWindows()
