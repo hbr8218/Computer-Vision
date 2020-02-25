a621223a4a657636817d361bde5b029b3ea1c46f
HASSAN BARI RAZA <hbr8218@gmail.com>
2020-02-25 18:09:52 +0530

    captures pictures from face cascade

--- cap.py
+++ cap.py
@@ -19,23 +19,21 @@ while True:
         break
     gray =cv2.cvtColor(frame,cv2.COLOR_BGR2GRAY)
     face = face_cascade.detectMultiScale(gray,1.3,6)
-    
+
     for (x,y,w,h) in face:
         cv2.rectangle(frame,(x,y),(x+w,y+h), (0,0,255), 2)
+
+        crop_gray = gray[y:y+h, x:x+w]
+        crop_color = frame[y:y+h, x:x+w]
         id += 1
-        image_name = 'my_pic' + str(id) +'.jpg'
-        cv2.imwrite(image_name, frame)
+        folder = "img/"
+        image_name = folder+'my_pic' + str(id) +'.jpg'
+        cv2.imwrite(image_name, crop_gray)
         cv2.imshow('Input',frame)
         print(image_name)
-    # total_frame += 1
-    # if total_frame%frameFrequency == 0:
-    #     id += 1
-    #     image_name = 'my_pic' + str(id) +'.jpg'
-    #     cv2.imwrite(image_name, frame)
-    #     cv2.imshow('Input',frame)
-    #     print(image_name)
+    
     c = cv2.waitKey(1)
     if c == 27:
         break
-cv2.release()
+# cv2.release()
 cv2.destroyAllWindows()
