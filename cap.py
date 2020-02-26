85e9fd3a5dcdc1df16d24807b59d163fd44d4f1b
HASSAN BARI RAZA <hbr8218@gmail.com>
2020-02-26 11:52:25 +0530

    cheking working of program

--- cap.py
+++ cap.py
@@ -2,25 +2,22 @@ import cv2
 import numpy
 
 face_cascade = cv2.CascadeClassifier('haarcascade_frontalface_default.xml')
-
-cap  = cv2.VideoCapture(0)
-total_frame = 0
 id = 0
-
+cap  = cv2.VideoCapture(0)
 if not cap.isOpened():
     exit(0)
 
-#Capture images per 25 frame
-frameFrequency=25
-
 while True:
+
     ret, frame = cap.read()
+    
     if ret is False:
         break
     gray =cv2.cvtColor(frame,cv2.COLOR_BGR2GRAY)
     face = face_cascade.detectMultiScale(gray,1.3,6)
 
     for (x,y,w,h) in face:
+        
         cv2.rectangle(frame,(x,y),(x+w,y+h), (0,0,255), 2)
 
         crop_gray = gray[y:y+h, x:x+w]
